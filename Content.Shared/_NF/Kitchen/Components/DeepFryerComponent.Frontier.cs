688b1e0684e39d93653941865a135ddc8b4ea23b
Milon <milonpl.git@proton.me>
2026-01-06 19:41:29 +0100

    deepfryer 2.0 (#5043)
    * was it worth the hype
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * minor stuff
    
    * multi-ingredient recipe support
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * oil transfer
    
    * explosions and fire
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * make the yaml linter happy
    
    * YML recipes
    
    * typo ops
    
    * review
    
    * Apply suggestions from code review
    
    Milon told me to.
    
    Signed-off-by: Vanessa <908648+ShepardToTheStars@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Vanessa <908648+ShepardToTheStars@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Stop-Signs <stopsign221@gmail.com>
    Co-authored-by: Vanessa <908648+ShepardToTheStars@users.noreply.github.com>

---
+++ Content.Shared/_NF/Kitchen/Components/DeepFryerComponent.Frontier.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Nyanotrasen.Kitchen.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._DV.Kitchen.Components;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+public sealed partial class DeepFryerComponent
+{
+    /// <summary>
+    /// Frontier: the crispiness level set to use for shaders, examination, etc.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public ProtoId<CrispinessLevelSetPrototype> CrispinessLevelSet { get; set; } = "Crispy";
+}
