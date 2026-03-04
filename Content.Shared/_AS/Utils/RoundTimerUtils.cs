ac602ecf3790d8512cf94ea6b2c54f30a03cf57c
Falcon <falcon@zigtag.dev>
2026-03-03 21:24:06 -0800

    refactor: replace all instances of round timer with our custom formatter

---
+++ Content.Shared/_AS/Utils/RoundTimerUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace Content.Shared._AS.Utils;
+
+/// <summary>
+/// Helper class to format the round timer for us
+/// </summary>
+public static class RoundTimerUtils
+{
+    public static string ToString(TimeSpan ts)
+    {
+        return $"{ts.TotalHours:00}:{ts:mm\\:ss}";
+    }
+}
