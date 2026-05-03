3b17795a6ba10071d52219e58a6369115e0a4c78
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2026-05-03 17:10:34 -0400

    Revert moving everything to shared. Functionally reverts to fbac23a27e93e3ad371c962455e1e8118e010161

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -111,7 +111,6 @@ public abstract partial class SharedGunSystem : EntitySystem
         InitializeContainer();
         InitializeSolution();
         InitializeGunExamine(); // Emberfall
-        InitializeAutoFire(); // Aurora - Move AutoFire to shared
 
         // Interactions
         SubscribeLocalEvent<GunComponent, GetVerbsEvent<AlternativeVerb>>(OnAltVerb);
